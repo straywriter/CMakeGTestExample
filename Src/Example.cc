bed15f3999e175d7c10901d1f7784df12eb27943
TA-MEI <2745982906@qq.com>
2020-12-07 12:51:12 +0800

    2020-12-7

---
+++ Src/Example.cc
@@ -0,0 +1,16 @@
+#include "Example.h"
+
+Example::Example(const char *name)
+        :name_(name) {
+}
+
+Example::~Example() {
+}
+
+std::string Example::Greet() const {
+    std::string ret;
+    ret.append("Hello, ");
+    ret.append(name_);
+    ret.push_back('!');
+    return ret;
+}
--- Test/Test.cpp
+++ Test/Test.cpp
@@ -2,47 +2,62 @@
 #include "gtest/gtest.h"
 
 #include "project.h"
+using namespace testing::internal;
 
+#define GTEST_COUT std::cerr << "[          ] [ INFO ]"
 
 // IndependentMethod is a test case - here, we have 2 tests for this 1 test case
-TEST(IndependentMethod, ResetsToZero) {
+TEST(IndependentMethod, ResetsToZero)
+{
 	int i = 3;
 	independentMethod(i);
 	EXPECT_EQ(0, i);
+	// EXPECT_TRUE(false) << "diagnostic message";
+
+// PRINTF("Hello world \n");
+//  TEST_COUT << "Hello world" << std::endl;
 
 	i = 12;
 	independentMethod(i);
-	EXPECT_EQ(0,i);
+	EXPECT_EQ(0, i);
 }
 
-TEST(IndependentMethod, ResetsToZero2) {
+TEST(IndependentMethod, ResetsToZero2)
+{
 	int i = 0;
+GTEST_COUT << "Hello World" << std::endl;
+std::cout<<"[info      ] \n";
 	independentMethod(i);
 	EXPECT_EQ(0, i);
 }
 
 // The fixture for testing class Project. From google test primer.
-class Project1Test : public ::testing::Test {
+class Project1Test : public ::testing::Test
+{
 protected:
 	// You can remove any or all of the following functions if its body
 	// is empty.
 
-	Project1Test() {
+	Project1Test()
+	{
 		// You can do set-up work for each test here.
 	}
 
-	virtual ~Project1Test() {
+	virtual ~Project1Test()
+	{
 		// You can do clean-up work that doesn't throw exceptions here.
 	}
 
 	// If the constructor and destructor are not enough for setting up
 	// and cleaning up each test, you can define the following methods:
-	virtual void SetUp() {
+	virtual void SetUp()
+	{
 		// Code here will be called immediately after the constructor (right
 		// before each test).
 	}
 
-	virtual void TearDown() {
+	virtual void TearDown()
+	{
 		// Code here will be called immediately after each test (right
 		// before the destructor).
 	}
@@ -53,7 +68,8 @@ protected:
 
 // Test case must be called the class above
 // Also note: use TEST_F instead of TEST to access the test fixture (from google test primer)
-TEST_F(Project1Test, MethodBarDoesAbc) {
+TEST_F(Project1Test, MethodBarDoesAbc)
+{
 	int i = 0;
 	p.foo(i); // we have access to p, declared in the fixture
 	EXPECT_EQ(1, i);
